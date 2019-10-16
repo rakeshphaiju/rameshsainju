766249816966a8d3892957fc5b211043a8e5e522
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2019-10-16 14:33:15 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-// Import React stuff
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, browserHistory, IndexRoute } from "react-router";
+import { Router, browserHistory, IndexRoute } from "react-router";
+import {Switch, Route} from "react-router-dom";
+
 // Import components
 import App from "./App";
 import Albums from "./components/album/Albums";
 import Album from "./components/album/Album";
-// import Photo from './components/Photo';
+
 import Cinematography from "./components/Cinematography";
 import About from "./components/About";
 import Contact from "./components/Contact";
-//import Header from "./components/layout/Header";
-//import Footer from "./components/layout/Footer";
+import NotFound from "./components/layout/NotFound";
+
 
 ReactDOM.render(
   <Router history={browserHistory}>
+    <Switch>
     <Route path="/" component={App}>
       <IndexRoute component={Albums} />
       <Route exact path="/cinematography" component={Cinematography} />
@@ -22,8 +24,9 @@ ReactDOM.render(
       <Route exact path="/contact" component={Contact} />
 
       <Route path=":albumName" component={Album} />
-    {/*  <Route path=":albumName/:photoId" component={Photo} /> */}
+     <Route component={NotFound}/>
     </Route>
+    </Switch>
   </Router>,
   document.getElementById("root")
 );

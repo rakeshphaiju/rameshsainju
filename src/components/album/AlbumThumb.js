77c1e66858eca891fa66a2a0d33025395487d267
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2020-09-15 14:06:20 +0300

    transition image

--- src/components/album/AlbumThumb.js
+++ src/components/album/AlbumThumb.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 const AlbumThumb = props => (
   <Link to={props.name} className="thumb">
-    <div className="thumbImage">
+    <div className="thumbImage fade">
       <img src={require('../../images/' + props.img)} alt={props.name} />
     </div>
     <h3 className="thumbTitle">{props.name}</h3>

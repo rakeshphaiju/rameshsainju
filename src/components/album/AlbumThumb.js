1b8a1d3bfa71ddd0dc558cae1ddf20bb44550edc
Rakesh Phaiju <rakeshphaiju22@gmail.com>
2020-09-23 14:11:47 +0300

    make it beautiful

--- src/components/album/AlbumThumb.js
+++ src/components/album/AlbumThumb.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Flip from 'react-reveal/Flip';
 
 const AlbumThumb = props => (
   <Link to={props.name} className="thumb">
     <div className="thumbImage fade">
+    <Flip left cascade>
       <img src={require('../../images/' + props.img)} alt={props.name} />
+      </Flip>
     </div>
     <h3 className="thumbTitle">{props.name}</h3>
   </Link>
